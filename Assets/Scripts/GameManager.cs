022aa030b4bcd13423602b2d7039ef2879c9de96
Dermody Jeran <jeran.dermody@nths-jdermody.belton.isd>
2015-09-11 10:20:52 -0500

    first commit pls

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : Photon.MonoBehaviour {
+	
+	public GameObject[] redSpawns;
+	public GameObject[] blueSpawns;
+	
+	public int state = 0;
+	
+	void Connect (){
+		PhotonNetwork.ConnectToBestCloudServer ("V1.0");
+		Connect ();
+	}
+	
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+	
+	void OnGUI () {
+		switch (state) {
+		case 0:
+			if (GUI.Button(new Rect(10, 10, 100, 30), "Connect")){
+				Connect ();
+			}
+			break;
+		case 1:
+			GUI.Label(new Rect(10, 10, 100, 30), "Connected");
+			break;
+		}
+	}
+}
